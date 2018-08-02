b7b0fe83f6065cc84e1c800e0e0f2d9fd550e5aa
jainakansha0017 <jainakansha0017@gmail.com>
2018-08-02 18:57:40 +0530

    handling error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-
+const morgan=require('morgan') // middleware to handle log , use next // show log in console
 const ProductRouter=require('./api/routes/products');
 const orderRouter=require('./api/routes/orders');
 
@@ -9,8 +9,23 @@ const orderRouter=require('./api/routes/orders');
 // 		message : "It works!"
 // 	});
 // });
-
+app.use(morgan('dev'));
 app.use('/products',ProductRouter);
 app.use('/orders',orderRouter);
+ //This line is approach if the url does not matches the above 2 lines
+app.use((req,res,next) => {
+	const error=new Error("Not Found"); // Error is an object provided by NOde js
+	error.status=404;
+	next(error); // propagate to app.use((error,req,res,next) => {
+});
+
+app.use((error,req,res,next) => {
+	res.status=(error.status || 500); // 500 error will never come from above comes as we manually given 404.500 will come from other operations 
+	res.json({
+		error:{
+			message:error.message
+		}
+	}); 
+});
 
 module.exports=app;
