14a594e264b895f2e84a13b1634e7efaa25b8d2e
Akansha Jain <jainakansha0017@gmail.com>
2018-08-03 22:06:11 +0530

    product post

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const router=express.Router();
+const mongoose=require('mongoose');
+const Product=require('../model/product')
 
 router.get('/',(req,res,next) => {
 	res.status(200).json({
@@ -8,32 +10,71 @@ router.get('/',(req,res,next) => {
 });
 
 router.post('/',(req,res,next) => {
-	const product={
+	// const product={
+	// 	name:req.body.name,
+	// 	price:req.body.price
+	// };
+	const product=new Product({
+		_id:new mongoose.Types.ObjectId(),
 		name:req.body.name,
 		price:req.body.price
-	};
-	res.status(201).json({
-		message :"Handling POST request to /products",
-		createdProduct:product
 	});
+	product
+		.save()
+		.then(result => {
+			console.log(result);
+			res.status(201).json({
+			message :"Handling POST request to /products",
+			createdProduct:product
+	});
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({
+				error:err
+			})
+		});
+	
 });
 
 router.get("/:ProductId",(req,res,next) => {
  	const id=req.params.ProductId;
- 	if(id==="special")
- 	{
- 		res.status(200).json({
- 			message: "Handling special id",
- 			id :id
- 		});
- 	}
- 	else
- 	{
- 		res.status(200).json({
- 			message: "You passed an ID",
- 			id :id
- 		});
- 	}
+ 	// if(id==="special")
+ 	// {
+ 	// 	res.status(200).json({
+ 	// 		message: "Handling special id",
+ 	// 		id :id
+ 	// 	});
+ 	// }
+ 	// else
+ 	// {
+ 	// 	res.status(200).json({
+ 	// 		message: "You passed an ID",
+ 	// 		id :id
+ 	// 	});
+ 	// }
+ 	Product.findById(id)
+ 		   .exec()
+ 		   .then(doc => {
+ 		   	  console.log(doc)
+ 		   	  if(doc)
+ 		   	  {
+ 		   	  	res.status(200).json(doc);
+ 		   	  }
+ 		   	  else
+ 		   	  {
+ 		   	  	res.status(404).json({
+ 		   	  		message:"No record found for given product ID"
+ 		   	  	});
+ 		   	  }
+ 		   	  
+ 		   })
+ 		   .catch(err => {
+ 		   		console.log(err);
+ 		   		res.status(500).json({
+ 		   			error:err
+ 		   		})
+ 		   });
  });
 
 router.patch('/:ProductId',(req,res,next) => {
