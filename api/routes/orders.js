d521fbceed7e5c034590982ccf06df75a2ee8d0e
Akansha Jain <jainakansha0017@gmail.com>
2018-08-15 22:22:09 +0530

    order complete

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -4,9 +4,30 @@ const mongoose=require('mongoose');
 const Order=require('../model/order')
 
 router.get('/',(req,res,next) => {
-	res.status(200).json({
-		message :"Orders are fetched"
-	});
+	Order.find()
+		 .select("producvt quantity _id")
+		 .exec()
+		 .then(doc => {
+		 	res.status(200).json({
+		 		count : doc.length,
+		 		orders : doc.map(d => {
+		 			return {
+		 				_id : d._id,
+		 				product : d.product,
+		 				quantity : d.quantity,
+		 				request : {
+		 					type : 'GET',
+		 					url : "http://localhost:3000/orders/"+d._id
+		 				}
+		 			}
+		 		})
+		 	});
+		 })
+		 .catch(err => {
+		 	res.status(500).json({
+		 		error:err
+		 	})
+		 })
 });
 
 router.post('/',(req,res,next) => {
@@ -21,7 +42,18 @@ router.post('/',(req,res,next) => {
 	// }
 	order.save()
 		 .then(result => {
-		 	res.status(201).json(result);
+		 	res.status(201).json({
+		 		message : 'Order Stored',
+		 		createdOrder : {
+		 			_id : result._id,
+		 			product : result.product,
+		 			quantity : result.quantity
+		 		},
+		 		request : {
+		 			type :'GET',
+		 			url : "http://localhost:3000/orders/"+result._id
+		 		}
+		 	});
 		 })
 		 .catch(err => {
 		 	res.status(500).json({
@@ -35,16 +67,44 @@ router.post('/',(req,res,next) => {
 });
 
 router.get("/:orderId",(req,res,next) => {
- 	res.status(200).json({
-		message :"Orders no: "+req.params.orderId +" fetched"
-	});
+ 	Order.findById(req.params.orderId)
+ 		 .exec()
+ 		 .then(order => {
+ 		 	if(!order)
+ 		 	{
+ 		 		return res.status(404).json({
+ 		 			msg : "Order not found"
+ 		 		})
+ 		 	}
+ 		 	res.status(200).json({
+ 		 		order :order,
+ 		 		request : {
+ 		 			type : "GET",
+ 		 			url : "http://localhost:3000/orders"
+ 		 		}
+ 		 	})
+ 		 })
+ 		 .catch(err => {
+ 		 	res.status(500).json({
+ 		 		errror : err
+ 		 	})
+ 		 })
  });
 
 
 router.delete('/:orderId',(req,res,next) => {
-	res.status(200).json({
-		message :"Deleting Order no: "+req.params.orderId
-	});
+	Order.remove({_id : req.params.orderId})
+		 .exec()
+		 .then(result => {
+		 	res.status(200).json({
+		 		msg : "order deleted"
+		 	})
+		 })
+		 .catch(err => {
+		 	res.status(500).json({
+		 		error : err
+		 	})
+		 })
 });
 
 
